774a15a110f58c60cceaf73ec05691911b839e48
JuRenGithub <ljrenwangyi@163.com>
2022-04-18 21:25:37 +0800

    format update

--- src/PipeLine.py
+++ src/PipeLine.py
@@ -17,31 +17,31 @@ device = torch.device('cuda')
 cpu = torch.device('cpu')
 
 
-
 class PipeLine:
-    def __init__(self, dataPath, trainData = None, trainInfo = None, valData = None, valInfo = None, resultWritter = "", args = {}, deviceID = 1, writeInfo = 1):
+    def __init__(self, dataPath, trainData=None, trainInfo=None, valData=None, valInfo=None, resultWritter="", args={},
+                 deviceID=1, writeInfo=1):
         self.resultWritter = resultWritter
         self.args = {}
-        
-        #Set manually
+
+        # Set manually
         self.args['numLayer'] = 3
         self.args['nclasses'] = [2, 6, 21]
         self.args['mlpPos'] = [0, 1, 2]
         self.args['baseModel'] = "DeepGraphlet"
         self.args['numIterator'] = 200
-        
+
         self.args['useRandomFeature'] = False
         self.args['useKTupleFeature'] = True
-        
+
         self.args['useDropout'] = True
         self.args['useBatchNorm'] = True
-        
-        #Grid Search
+
+        # Grid Search
         self.args['learningRate'] = 0.001
         self.args['weightDecay'] = 0
         self.args['keepProb'] = 0.5
-        
-        #nearly fixed parameters
+
+        # nearly fixed parameters
         self.args['aggregator'] = "mean"
         self.args['activateFunc'] = "relu"
         self.args['hiddenDim'] = 128
@@ -49,19 +49,19 @@ class PipeLine:
         self.args['loss'] = "kl"
         for key, value in args.items():
             self.args[key] = value
-        
+
         # if self.args['useRandomFeature'] == True:
         #     self.args['nodeFeatureDim'] = 29
         # if self.args['useKTupleFeature'] == True:
         #     self.args['nodeFeatureDim'] = 29
-        
+
         print("featureDim: ", self.args['nodeFeatureDim'])
-        
+
         self.writeInfo = str(writeInfo)
-        
+
         if deviceID >= 0:
             torch.cuda.set_device(deviceID)
-            
+
         self.trainData = {}
         self.trainInfo = {}
         self.valData = {}
@@ -73,16 +73,16 @@ class PipeLine:
             self.valInfo = valInfo
 
         if self.args['loss'] == "kl":
-            self.criterion = nn.KLDivLoss(reduction = 'batchmean')
+            self.criterion = nn.KLDivLoss(reduction='batchmean')
         elif self.args['loss'] == "mse":
             self.criterion = RMSE()
-        
+
         print('trainVal split')
         filePath, valPath = self.trainVal(dataPath)
         self.trainData, self.trainInfo = self.loadData(filePath)
         self.valData, self.valInfo = self.loadData(valPath)
         print(len(self.trainData), len(self.valData))
-        
+
     def load_args(self, args):
         for key, value in args.items():
             self.args[key] = value
@@ -91,32 +91,41 @@ class PipeLine:
         else:
             self.args['nodeFeatureDim'] = 1
         if self.args['loss'] == "kl":
-            self.criterion = nn.KLDivLoss(reduction = 'batchmean')
+            self.criterion = nn.KLDivLoss(reduction='batchmean')
         elif self.args['loss'] == "mse":
             self.criterion = RMSE()
 
         if self.args['baseModel'] == "DeepGraphlet":
-            self.model = DeepGraphlet(nfeat = self.args['nodeFeatureDim'], nhid = self.args['hiddenDim'], nlayer = self.args['numLayer'], nclasses = self.args['nclasses'], mlpPos = self.args['mlpPos'], useDropout = self.args['useDropout'], keepProb =self.args['keepProb'], useBatchNorm = self.args['useBatchNorm'])
+            self.model = DeepGraphlet(nfeat=self.args['nodeFeatureDim'], nhid=self.args['hiddenDim'],
+                                      nlayer=self.args['numLayer'], nclasses=self.args['nclasses'],
+                                      mlpPos=self.args['mlpPos'], useDropout=self.args['useDropout'],
+                                      keepProb=self.args['keepProb'], useBatchNorm=self.args['useBatchNorm'])
         elif self.args['baseModel'] == "GIN":
-            self.model = GIN(nfeat = self.args['nodeFeatureDim'], nhid = self.args['hiddenDim'], nlayer = self.args['numLayer'], nclasses = self.args['nclasses'], mlpPos = self.args['mlpPos'], useDropout = self.args['useDropout'], keepProb =self.args['keepProb'], useBatchNorm = self.args['useBatchNorm'])
+            self.model = GIN(nfeat=self.args['nodeFeatureDim'], nhid=self.args['hiddenDim'],
+                             nlayer=self.args['numLayer'], nclasses=self.args['nclasses'], mlpPos=self.args['mlpPos'],
+                             useDropout=self.args['useDropout'], keepProb=self.args['keepProb'],
+                             useBatchNorm=self.args['useBatchNorm'])
         elif self.args['baseModel'] == "GCN":
-            self.model = GCN(nfeat = self.args['nodeFeatureDim'], nhid = self.args['hiddenDim'], nlayer = self.args['numLayer'], nclasses = self.args['nclasses'], mlpPos = self.args['mlpPos'], useDropout = self.args['useDropout'], keepProb =self.args['keepProb'], useBatchNorm = self.args['useBatchNorm'])
+            self.model = GCN(nfeat=self.args['nodeFeatureDim'], nhid=self.args['hiddenDim'],
+                             nlayer=self.args['numLayer'], nclasses=self.args['nclasses'], mlpPos=self.args['mlpPos'],
+                             useDropout=self.args['useDropout'], keepProb=self.args['keepProb'],
+                             useBatchNorm=self.args['useBatchNorm'])
         elif self.args['baseModel'] == "KMLP":
-            self.model = KMLP(self.args['nodeFeatureDim'], self.args['hiddenDim'], [2,6,21],useDropout = self.args['useDropout'], keepProb =self.args['keepProb'], useBatchNorm = self.args['useBatchNorm'])
- 
+            self.model = KMLP(self.args['nodeFeatureDim'], self.args['hiddenDim'], [2, 6, 21],
+                              useDropout=self.args['useDropout'], keepProb=self.args['keepProb'],
+                              useBatchNorm=self.args['useBatchNorm'])
 
     def load_result_writter(self, result_writter):
         self.resultWritter = result_writter
-        
+
     def SaveModel(self, modelPath):
         torch.save(self.model.state_dict(), modelPath)
-        
+
     def LoadModel(self, modelPath):
-        self.model.load_state_dict(torch.load(modelPath,map_location=torch.device('cpu')))
+        self.model.load_state_dict(torch.load(modelPath, map_location=torch.device('cpu')))
 
         # self.model.load_state_dict(torch.load(modelPath,,map_location=torch.device('cpu')))
 
-    
     def GenerateLabel(self, nodeCnt, path):
         fileName = path.split('/')[-1].split('.')[0]
         items = path.split('/')
@@ -124,30 +133,29 @@ class PipeLine:
         for i in range(len(items) - 1):
             prefix += items[i] + "/"
         outFile = prefix + fileName + ".out"
-        df = pd.read_csv(outFile, sep=' ',header = None).iloc[:, :-1]
+        df = pd.read_csv(outFile, sep=' ', header=None).iloc[:, :-1]
         rawLabel = np.array(df)
-    
-        
-        orbits = [1,    2, 1,   2, 2, 1, 3, 2, 1,    3, 4, 2, 3, 4, 3, 1, 4, 4, 2, 4, 2, 3, 2, 3, 3, 3, 3, 2, 2, 1]
-        motifs = np.zeros((nodeCnt, 30), dtype = np.float64)
+
+        orbits = [1, 2, 1, 2, 2, 1, 3, 2, 1, 3, 4, 2, 3, 4, 3, 1, 4, 4, 2, 4, 2, 3, 2, 3, 3, 3, 3, 2, 2, 1]
+        motifs = np.zeros((nodeCnt, 30), dtype=np.float64)
         cnt = 0
         st = 0
         for length in orbits:
-            motifs[:, cnt] = np.sum(rawLabel[:, st : st + orbits[cnt]], axis = 1)
+            motifs[:, cnt] = np.sum(rawLabel[:, st: st + orbits[cnt]], axis=1)
             st += orbits[cnt]
             cnt += 1
         labels = []
-        labels.append(motifs[:, 1 : 3])
-        labels.append(motifs[:, 3 : 9])
-        labels.append(motifs[:, 9 : 30])
+        labels.append(motifs[:, 1: 3])
+        labels.append(motifs[:, 3: 9])
+        labels.append(motifs[:, 9: 30])
         for i in range(len(labels)):
             labels[i] += 1e-10
-            labels[i] = labels[i] / np.sum(labels[i], axis = 1).reshape((labels[i].shape[0], 1))
+            labels[i] = labels[i] / np.sum(labels[i], axis=1).reshape((labels[i].shape[0], 1))
         return labels
 
-    def GenerateFeedDict(self, fileName = "test", needLabel = True, cpu = False):
-        
-        #Do this edge have been cleaned ?
+    def GenerateFeedDict(self, fileName="test", needLabel=True, cpu=False):
+
+        # Do this edge have been cleaned ?
         nodeCnt, edgeCnt, edgeList = readEdgeList(fileName)
         nodeFeature = {}
         nodeFeature['raw'] = torch.from_numpy(np.ones((nodeCnt, 1))).float()
@@ -156,9 +164,9 @@ class PipeLine:
         # elif self.args['useKTupleFeature'] == True:
         prefix, _ = split_between_last_char(fileName, '.')
         nodeFeature['ktuple'] = torch.from_numpy(np.loadtxt(prefix + ".edges_features5") / 100).float()
-        #???
-            
-        indices = np.zeros((2, edgeCnt * 2), dtype = np.int64)
+        # ???
+
+        indices = np.zeros((2, edgeCnt * 2), dtype=np.int64)
         # deg = []
         # if self.args['baseModel'] == "GCN" or self.args['aggregator'] == 'GCN' or self.args['aggregator'] == "mean":
         deg = [0 for i in range(nodeCnt)]
@@ -169,7 +177,7 @@ class PipeLine:
         for i in range(nodeCnt):
             if deg[i] == 0:
                 deg[i] = 1
-#         jishu = 0
+        #         jishu = 0
 
         # if self.args['aggregator'] == "sum":
         #     values[edgeCnt] = 1.0
@@ -179,7 +187,7 @@ class PipeLine:
         #     values[edgeCnt] = 1.0 / deg[int(u)] / deg[int(v)]
         adj = {}
         for aggr in ["sum", "mean", "GCN"]:
-            values = np.zeros((edgeCnt * 2), dtype = np.float32)
+            values = np.zeros((edgeCnt * 2), dtype=np.float32)
             cnt = 0
             for edge in edgeList:
                 u, v = edge
@@ -191,7 +199,7 @@ class PipeLine:
                 elif aggr == "GCN":
                     values[cnt] = 1.0 / deg[int(u)] / deg[int(v)]
                 cnt += 1
-                
+
                 v, u = edge
                 indices[0, cnt], indices[1, cnt] = u, v
                 if aggr == "sum":
@@ -201,7 +209,8 @@ class PipeLine:
                 elif aggr == "GCN":
                     values[cnt] = 1.0 / deg[int(u)] / deg[int(v)]
                 cnt += 1
-            sparseAggregator = torch.sparse.FloatTensor(torch.from_numpy(indices), torch.from_numpy(values), torch.Size([nodeCnt, nodeCnt]))
+            sparseAggregator = torch.sparse.FloatTensor(torch.from_numpy(indices), torch.from_numpy(values),
+                                                        torch.Size([nodeCnt, nodeCnt]))
             sparseAggregator = sparseAggregator.float()
             adj[aggr] = sparseAggregator
         labels = []
@@ -209,36 +218,31 @@ class PipeLine:
             labels = self.GenerateLabel(nodeCnt, fileName)
             for i in range(len(labels)):
                 labels[i] = torch.from_numpy(labels[i]).float()
-        
-        
+
         # nodeFeature = torch.from_numpy(nodeFeature).float()
-            
+
         edgeIndex = (torch.from_numpy(np.array(edgeList).T.astype(int))).type(torch.LongTensor)
         return nodeFeature, edgeIndex, adj, labels, nodeCnt, edgeCnt
-    
-    
-    
-    def loadData(self, filePath, needLabel = True, cpu = False):
+
+    def loadData(self, filePath, needLabel=True, cpu=False):
         idx = 0
         dataDic = {}
         infoDic = {}
         for path in filePath:
-            features, edgeIndex, adj, labels, nodeCnt, edgeCnt = self.GenerateFeedDict(fileName = path, needLabel = needLabel, cpu = False)
+            features, edgeIndex, adj, labels, nodeCnt, edgeCnt = self.GenerateFeedDict(fileName=path,
+                                                                                       needLabel=needLabel, cpu=False)
             dataDic[idx] = (features, edgeIndex, adj, labels)
             infoDic[idx] = (nodeCnt, edgeCnt)
             idx += 1
         return dataDic, infoDic
 
-
-        
     def train(self, features, adj, labels):
         # print(features[0])
         self.model.train()
         self.optimizer.zero_grad()
-        
+
         preds = self.model(features, adj)
-        
-            
+
         losses = []
         loss = 0
         for i in range(len(preds)):
@@ -248,13 +252,12 @@ class PipeLine:
                 idx = 1
             elif self.args['nclasses'][i] == 21:
                 idx = 2
-            
-            
+
             if self.args['loss'] == "kl":
-                cur_loss = self.criterion(F.log_softmax(preds[i], dim = 1), labels[idx])
-                preds[i] = F.softmax(preds[i], dim = 1)
+                cur_loss = self.criterion(F.log_softmax(preds[i], dim=1), labels[idx])
+                preds[i] = F.softmax(preds[i], dim=1)
             else:
-                preds[i] = F.softmax(preds[i], dim = 1)
+                preds[i] = F.softmax(preds[i], dim=1)
                 cur_loss = self.criterion(preds[i], labels[idx])
             # preds[i] = F.softmax(preds[i], dim = 1)
             # if self.args['loss'] == "kl":
@@ -265,14 +268,13 @@ class PipeLine:
             losses.append(cur_loss)
         loss.backward()
         self.optimizer.step()
-        
+
         return losses, preds, labels
-    
-    
+
     def eval(self, features, adj, labels, criterion):
         with torch.no_grad():
             self.model.eval()
-            
+
             preds = self.model(features, adj)
             losses = []
             loss = 0
@@ -284,12 +286,11 @@ class PipeLine:
                 elif self.args['nclasses'][i] == 21:
                     idx = 2
 
-                
                 if self.args['loss'] == "kl":
-                    cur_loss = self.criterion(F.log_softmax(preds[i], dim = 1), labels[idx])
-                    preds[i] = F.softmax(preds[i], dim = 1)
+                    cur_loss = self.criterion(F.log_softmax(preds[i], dim=1), labels[idx])
+                    preds[i] = F.softmax(preds[i], dim=1)
                 else:
-                    preds[i] = F.softmax(preds[i], dim = 1)
+                    preds[i] = F.softmax(preds[i], dim=1)
                     cur_loss = self.criterion(preds[i], labels[idx])
                 loss = loss + cur_loss
                 losses.append(cur_loss)
@@ -312,17 +313,17 @@ class PipeLine:
             filePath.extend(fileNames[:trainNum])
             valPath.extend(fileNames[trainNum:])
         return filePath, valPath
-    
+
     def torchList2floatList(self, losses):
         results = []
         for loss in losses:
             results.append(float(loss.to(cpu)))
         return results
-                
-                
+
     def train_graph(self, modelPath):
         self.model.to(device)
-        self.optimizer = optim.Adam(self.model.parameters(), lr = self.args['learningRate'], weight_decay = self.args['weightDecay'])
+        self.optimizer = optim.Adam(self.model.parameters(), lr=self.args['learningRate'],
+                                    weight_decay=self.args['weightDecay'])
 
         print(len(self.trainData))
         print(self.args)
@@ -346,7 +347,7 @@ class PipeLine:
             adj = adj[self.args['aggregator']]
             features, adj = features.to(device), adj.to(device)
             for j in range(len(labels)):
-                 labels[j] = labels[j].to(device)
+                labels[j] = labels[j].to(device)
 
             result = self.train(features, adj, labels)
             if (i % step == 0) or (i == self.args['numIterator'] - 1):
@@ -354,22 +355,22 @@ class PipeLine:
                 print(i)
                 print(self.trainInfo[idx])
                 print(result[0])
-                valScore, valLosses = self.testRealGraph(printInfo = False)
+                valScore, valLosses = self.testRealGraph(printInfo=False)
                 print(valLosses)
                 if valScore < bestValScore:
                     bestValScore = valScore
                     bestValLosses = valLosses
                     self.SaveModel(modelPath)
-            
-            #is this trans needed???
+
+            # is this trans needed???
             features, adj = features.to(cpu), adj.to(cpu)
             for j in range(len(labels)):
-                 labels[j] = labels[j].to(cpu)
+                labels[j] = labels[j].to(cpu)
         print("best valScore: ", bestValScore)
         print("best valLosses: ", bestValLosses)
         self.resultWritter.writeResult('------------------------------------------------------------------------')
         time_end = time.time()
-        print("time cost",time_end - time_start,'s')
+        print("time cost", time_end - time_start, 's')
         self.resultWritter.writeResult("time cost" + str(time_end - time_start) + 's')
         self.resultWritter.writeResult("trainDataNum: " + str(len(self.trainData)))
         self.resultWritter.writeResult("valDataNum: " + str(len(self.valData)))
@@ -378,15 +379,15 @@ class PipeLine:
         self.resultWritter.writeResult("bestValLosses: ")
         self.resultWritter.saveListLine(self.torchList2floatList(bestValLosses))
         return bestValScore
-    
-    def testRealGraph(self, filePath = "", printInfo = True):
+
+    def testRealGraph(self, filePath="", printInfo=True):
         valScore = 0
         lossResult = None
         for i in range(len(self.valData)):
             if printInfo:
                 self.resultWritter.saveList(self.valInfo[i])
                 print(self.valInfo[i])
-            
+
             features, _, adj, labels = self.valData[i]
 
             adj = adj[self.args['aggregator']]
@@ -414,13 +415,12 @@ class PipeLine:
         for i in range(len(lossResult)):
             lossResult[i] /= len(self.valData)
         return valScore / len(self.valData), lossResult
-    
-    
-    def load_infer_data(self, valData, needLabel = True):
-        self.inferData, self.inferInfo = self.loadData(valData, needLabel = needLabel)
+
+    def load_infer_data(self, valData, needLabel=True):
+        self.inferData, self.inferInfo = self.loadData(valData, needLabel=needLabel)
         self.inferPath = valData
 
-    def inferRealGraph(self, filePath = "", printInfo = True, needLabel = True, write = False):
+    def inferRealGraph(self, filePath="", printInfo=True, needLabel=True, write=False):
         print("Enter LoadData")
         self.resultWritter.saveList(self.inferPath)
         self.model.to(cpu)
